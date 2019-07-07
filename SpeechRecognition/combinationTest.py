5626a52b4898db7e305fca8ae3cd660254e66b9c
soul-jah <souljah@tutanota.com>
2019-07-07 21:24:25 +0900

    Begining subsystem implementation, language translation in progress - DE

--- SpeechRecognition/combinationTest.py
+++ SpeechRecognition/combinationTest.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
 import random
+import webbrowser
+
+engine = pyttsx3.init()
+r = sr.Recognizer()
 
 def greeting():
 
@@ -10,25 +14,60 @@ def greeting():
                     "How's your day going?", "Good to see you!", \
                     "Nice to see you", "Long time no see", "It's been a while"]
 
-    engine = pyttsx3.init()
-    r = sr.Recognizer()
-
     print(len(greetingList))
 
     with sr.Microphone() as source:
         print('Speak: ')
         audio = r.listen(source)
         text = r.recognize_google(audio)
-        term = "hello"
+        greetingTerm = "hello"
         words = text.split()
-        randomGreeting = str(greetingList[random.randint(0,15)]) + "Friend"
-        if term in words:
+        randomGreeting = str(greetingList[random.randint(0,15)]) + "Leo Rose"
+        if greetingTerm in words:
             engine.say(randomGreeting)
             engine.runAndWait()
         else:
             print("Error")
 
+def farewell():
+
+    farewellList = ["bye", "bye bye", "See you later", "See you soon", \
+                    "Talk to you later", "I must be going", "Take it easy"\
+                    "I'm off too", "Goodbye", "Have a good day",\
+                    "Have a nice day", "I'll look forward to our next meeting"\
+                    "Until next time", "Take care", "It was nice talking to you"\
+                    "It was nice seeing you", "Until later", "Peace out",\
+                     "I'm out", "I'm off", ]
+    print(len(farewellList))
+
+    with sr.Microphone() as source:
+        print('Speak: ')
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        farewellTerm = "bye"
+        words = text.split()
+        randomFarewell = str(farewellList[random.randint(0,15)]) + "Leo Rose"
+        if farewellTerm in words:
+            engine.say(randomFarewell)
+            engine.runAndWait()
+        else:
+            print("Error")
+
+def executeCommand():
+    webbrowser.open('https://www.youtube.com/watch?v=0xiB_S7NTUY')
+
 def request():
+    introOffer = ["What can I help with today?", "What can I do for you?",\
+                  "What will you have me do?"]
+    randomOffer = str(introOffer[random.randint(0,2)])
+    engine.say(randomOffer)
+    engine.runAndWait()
 
-
+print("Greeting")
 greeting()
+print("Request")
+request()
+print("Execute Command")
+executeCommand()
+print("Farewell")
+farewell()
---
+++ SpeechRecognition/phrases_DE.py
@@ -0,0 +1,13 @@
+greetingList = ["Hallo", "Gutten Tag", "Wie geht's?", "Wie geht es Ihnen?"\
+                "Wie ist alles", "Wie läuft dein Tag", "schön dich zu sehen"\
+                "schön dich wieder zu sehen", "lange nicht gesehen", \
+                "Es ist eine Weile her"]
+
+farewellList = ["Tschüss", "bis später", "bis bald", "Wir sprechen später"\
+                "Ich muss gehen", "Nimm es nicht so schwer", "Ich bin weg"\
+                "Auf Wiedersehen", "haben Sie einen guten Tag",\
+                "Einen schönen Tag noch", "Ich freue mich auf unser nächstes Treffen"\
+                "es war schön dich wieder zu sehen", "bis zum nächsten Mal"\]
+
+introOffer = ["Womit kann ich helfen?", "Was kann ich für Dich tun?",\
+              "Was soll ich tun?", "Wie kann ich Ihnen heute helfen?"]
