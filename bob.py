0f22d30a989cd286c1df34ba65eeef49716b3181
soul-jah <souljah@tutanota.com>
2019-07-11 05:37:33 +0900

    Clean up bob

--- bob.py
+++ bob.py
@@ -1,6 +1,4 @@
-#from SpeechRecognition import languages
 from SpeechRecognition import listening
-#from FacialRecognition import facialRecognition
 
 ###############################################################################
 #                                                                             #
@@ -11,15 +9,7 @@ from SpeechRecognition import listening
 # Introduction
 #listening.bob.say("I, am Bob... And soon, I, will be self aware.. ")
 #listening.bob.runAndWait()
-# Debug count initialization
-i = 1
 
-#Start main loop
 while True:
-
     #Start bob listning for input
     listening.thinking(listening.listen())
-
-    # Debug check for execution ordering
-    print("phase",i,"finished")
-    i = i + 1
